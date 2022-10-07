be1bd70c097a8adcb9c35e26094faa2a1376cf54
Atul Asati <iampydev2020@gmail.com>
2022-10-07 11:40:10 -0700

    jitsi modal box integration

--- src/pages/auth/utils.js
+++ src/pages/auth/utils.js
@@ -3,11 +3,13 @@ localStorage.setItem("localStorageGroup", JSON.stringify(localStorageGroup));
 function getHost() {
   var HOST = "";
   if (window.location.origin.includes("localhost")) {
-    HOST = "http://localhost:8000";
-    console.log(HOST);
+    HOST = "http://192.168.29.147:8000";
+
+    console.log(HOST,"************************CURRENT DOMAIN********************");
   } else {
-    HOST = "http://18.117.227.68:8041"
+    HOST = "http://192.168.29.147:8000"
     // HOST = "https://api.dreampotential.org";
+    console.log(HOST,"************************CURRENT DOMAIN********************");
   }
   return HOST;
 }
@@ -15,9 +17,9 @@ function getHost() {
 function getServer() {
   var SERVER = "";
   if (window.location.origin.includes("localhost")) {
-    SERVER = "http://localhost:8040/";
+    SERVER = "http://192.168.29.147:8000";
   } else {
-    SERVER = "https://18.117.227.68:8041";
+    SERVER = "http://192.168.29.147:8000";
     // SERVER = "https://api.dreampotential.org";
   }
   return SERVER;
@@ -26,10 +28,10 @@ function getServer() {
 function getWebsocketHost() {
   var WEBSOCKET_HOST = "";
   if (window.location.origin.includes("localhost")) {
-    WEBSOCKET_HOST = "ws://localhost:8000";
+    WEBSOCKET_HOST = "ws://192.168.29.147:8000";
   } else {
     // WEBSOCKET_HOST = "wss://api.dreampotential.org";
-    WEBSOCKET_HOST = 'ws://18.117.227.68:8041';
+    WEBSOCKET_HOST = 'ws://192.168.29.147:8000';
   }
   return WEBSOCKET_HOST;
 }
